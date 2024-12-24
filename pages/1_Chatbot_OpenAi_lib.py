73cec2eb8e9cf32a2018fb14405077afd357a4a8
Hasan Ghaffari <53246858+hasanghaffari93@users.noreply.github.com>
2024-12-25 00:47:32 +0330

    bring chat contig to sidebar

--- pages/1_Chatbot_OpenAi_lib.py
+++ pages/1_Chatbot_OpenAi_lib.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from openai import OpenAI
+import openai
 
 # Constants
 API_PROVIDERS = ('OpenAI', 'Groq')
@@ -25,54 +25,102 @@ def restart_chat():
 st.title("🤖 Chatbot with OpenAI Library")
 st.caption("🚀 Let's chat with different models using OpenAI's client libraries")
 
+
+# Sidebar
+api_provider = st.sidebar.selectbox(
+    label="Which API do you want to use?",
+    options=API_PROVIDERS,
+    index=0,
+    key="api_provider",
+    placeholder="Select an API...",
+    on_change=restart_chat,
+)
+
+model_name = st.sidebar.selectbox(
+    label="Which model do you want to use?",
+    options=MODEL_NAMES[api_provider],
+    index=0,
+    key="model_name",
+    placeholder="Select a model...",
+    on_change=restart_chat,
+    )
+
+auth_type = st.sidebar.radio(
+    "How would you like to authenticate?",
+    ["Use an API Key", "Use a Password"],
+)
+
+if auth_type == "Use an API Key":
+    label = "{} API Key:".format(api_provider)
+else:
+    label = "Password:"
+
+auth = st.sidebar.text_input(
+    label=label,
+    key="auth",
+    on_change=restart_chat,
+    type="password",
+)
+
+@st.cache_data()
+def check_openai_api_key(api_key):
+    client = openai.OpenAI(
+        base_url=BASE_URLS[api_provider],
+        api_key=api_key,
+    )
+    try:
+        models = client.models.list()
+    except openai.AuthenticationError:
+        return False
+    except Exception as error:
+        st.sidebar.error(error)
+    else:
+        return True
+    
+
+st.session_state["valid_auth"] = False
+
+if auth_type == "Use an API Key":
+    if check_openai_api_key(auth):
+        api_key = auth
+        st.session_state["valid_auth"] = True
+        st.sidebar.success("Valid API key")
+    else:
+        st.sidebar.error("Invalid API key")
+else:
+    if auth in st.secrets["PASSWORDS"]:
+        api_key = st.secrets[KEY_NAMES[api_provider]]
+        st.session_state["valid_auth"] = True
+        st.sidebar.success("Valid password")
+    else:
+        st.sidebar.error("Invalid password")
+
+
+
 # Layout
-col1, col2 = st.columns(2)
+st.text_input(
+    label="System Prompt:",
+    value="You are a helpful assistant.",
+    max_chars=100,
+    key="system_prompt",
+    help="Top-level instructions for the model's behavior",
+    placeholder="System Prompt",
+    on_change=restart_chat,
+)
 
-with col1:
-    api_provider = st.selectbox(
-        label="Which API do you want to use?",
-        options=API_PROVIDERS,
-        index=0,
-        key="api_provider",
-        help="This app has API key of these providers",
-        placeholder="Select an API...",
-        on_change=restart_chat,
-    )
-
-    if api_provider:
-        model_name = st.selectbox(
-            label="Which model do you want to use?",
-            options=MODEL_NAMES[api_provider],
-            index=0,
-            key="model_name",
-            help="This app has API key of these model",
-            placeholder="Select a model...",
-            on_change=restart_chat,
-        )
-
-with col2:
-    st.text_input(
-        label="System Prompt:",
-        value="You are a helpful assistant.",
-        max_chars=100,
-        key="system_prompt",
-        help="Top-level instructions for the model's behavior",
-        placeholder="System Prompt",
-        on_change=restart_chat,
-    )
-
-    stream_enabled = st.checkbox(
-        label="Enable stream chat",
-        value=True,
-        key="stream",
-        help="The output will be streaming",
-    )
+stream_enabled = st.checkbox(
+    label="Enable stream chat",
+    value=True,
+    key="stream",
+    help="The output will be streaming",
+)
 
 # Initialize OpenAI client
-client = OpenAI(
-    base_url=BASE_URLS[api_provider],
-    api_key=st.secrets[KEY_NAMES[api_provider]],
-)
+if st.session_state["valid_auth"]:
+    client = openai.OpenAI(
+        base_url=BASE_URLS[api_provider],
+        api_key=api_key,
+    )
 
 # Initialize chat messages
 if "messages" not in st.session_state:
@@ -90,7 +138,7 @@ for msg in st.session_state.messages[1:]:
     st.chat_message(msg["role"]).write(msg["content"])
 
 # Handle new messages
-if prompt := st.chat_input():
+if prompt := st.chat_input(disabled=not st.session_state["valid_auth"]):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message(name="user").write(prompt)
 
