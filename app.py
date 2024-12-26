f434149c821b903d08594d8a6f7d967afb1e0a45
Hasan Ghaffari <53246858+hasanghaffari93@users.noreply.github.com>
2024-12-26 14:51:26 +0330

    change entrypoint file to handle multipage

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+
+
+# Each page is a script executed from your entrypoint file. \
+# You can define a page from a Python file or function. 
+# If you include elements or widgets in your entrypoint file, 
+# they become common elements between your pages.
+
+# TODO: Find good icon from https://fonts.google.com/icons?icon.set=Material+Symbols&icon.style=Rounded
+
+pg = st.navigation({
+    "Chatbots": [
+        st.Page("pages/0_Chatbot_LangChain.py", title="Chatbot Langchain", icon=":material/dashboard:")
+    ],
+    "Summarization": [
+        st.Page("pages/1_Summarization_Stuff.py", title="Stuff Technique", icon=":material/dashboard:"),
+        st.Page("pages/2_Summarization_Map_Reduce.py", title="Map-Reduce Technique", icon=":material/dashboard:")
+    ],
+    "RAG": [
+        st.Page("pages/3_RAG.py", title="PDF Q/A", icon=":material/dashboard:")
+    ]
+})
+
+pg.run()
